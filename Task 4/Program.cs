1e4e96bcf17989bf154325ff180f48095e588940
{{w84d8}} <{{rakonline99@gmail.com}}>
2023-11-30 15:06:51 +0200

    4

---
+++ Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        TaskScheduler<string, int> scheduler = new TaskScheduler<string, int>(
+            task => int.Parse(task.Split(',')[0]), // Priority selector
+            task => Console.WriteLine($"Initializing task: {task}"), // Initialize task
+            task => Console.WriteLine($"Resetting task: {task}") // Reset task
+        );
+
+        Console.WriteLine("Enter tasks (format: Priority,Task; enter 'exit' to finish):");
+        string input;
+
+        do
+        {
+            input = Console.ReadLine();
+
+            if (input != "exit")
+            {
+                scheduler.EnqueueTask(input);
+            }
+
+        } while (input != "exit");
+
+        Console.WriteLine("Executing tasks:");
+
+        while (scheduler.GetTaskQueue().Count > 0)
+        {
+            scheduler.ExecuteNext(task => Console.WriteLine($"Executing task: {task}"));
+        }
+    }
+}
---
+++ Task 4/TaskScheduler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TaskScheduler<TTask, TPriority> where TPriority : notnull
+{
+    private readonly SortedDictionary<TPriority, Queue<TTask>> taskQueue = new SortedDictionary<TPriority, Queue<TTask>>();
+    private readonly Func<TTask, TPriority> prioritySelector;
+    private readonly Action<TTask> initializeTask;
+    private readonly Action<TTask> resetTask;
+
+    public TaskScheduler(Func<TTask, TPriority> prioritySelector, Action<TTask> initializeTask, Action<TTask> resetTask)
+    {
+        this.prioritySelector = prioritySelector ?? throw new ArgumentNullException(nameof(prioritySelector));
+        this.initializeTask = initializeTask ?? throw new ArgumentNullException(nameof(initializeTask));
+        this.resetTask = resetTask ?? throw new ArgumentNullException(nameof(resetTask));
+    }
+
+    public void EnqueueTask(TTask task)
+    {
+        TPriority priority = prioritySelector(task);
+
+        if (!taskQueue.ContainsKey(priority))
+        {
+            taskQueue[priority] = new Queue<TTask>();
+        }
+
+        taskQueue[priority].Enqueue(task);
+    }
+
+    public SortedDictionary<TPriority, Queue<TTask>> GetTaskQueue()
+    {
+        return new SortedDictionary<TPriority, Queue<TTask>>(taskQueue);
+    }
+
+    public void ExecuteNext(Action<TTask> taskExecutor)
+    {
+        if (taskQueue.Count > 0)
+        {
+            TPriority highestPriority = taskQueue.Keys.Max();
+            TTask nextTask = taskQueue[highestPriority].Dequeue();
+
+            initializeTask(nextTask);
+            taskExecutor(nextTask);
+            resetTask(nextTask);
+
+            if (taskQueue[highestPriority].Count == 0)
+            {
+                taskQueue.Remove(highestPriority);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No tasks in the queue.");
+        }
+    }
+}
